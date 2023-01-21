5be48a3181e0cba3a8ee6946ea51d61a1fc7530e
yu-ko-ba <yu-ko-ba@outlook.com>
2023-01-21 15:21:11 +0900

    たぶんできた

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import { Button, Container, TextField, Typography } from '@mui/material'
+import Head from 'next/head'
+import React, { useState } from 'react'
 
 export default function Home() {
+  const [ymUrl, setYmUrl] = useState("")
+  const [shareUrl, setShareUrl] = useState("")
   return (
-    <>
-    </>
+    <Container maxWidth="xs">
+      <Head>
+        <title>YM Previewer for Twitter</title>
+      </Head>
+      <Typography>シェアしたいYouTube MusicのURLを入力してください</Typography>
+      <TextField
+        label="URL"
+        value={ymUrl}
+        onChange={(e) => {
+          setYmUrl(e.target.value)
+          setShareUrl(`https://ym-previewer.vercel.app/${e.target.value.slice(34)}`)
+        }}
+        multiline
+        fullWidth
+      />
+      <Typography>こちらのURLをツイートに貼ってください</Typography>
+      <Typography>{shareUrl}</Typography>
+      <Button
+        variant="contained"
+        onClick={() => {
+          try {
+            navigator.share({ url: shareUrl })
+              .catch((err: Error) => {
+                if (process.env.NODE_ENV === "development") {
+                  console.log(err)
+                }
+              })
+          } catch (err) {
+            if (process.env.NODE_ENV === "development") {
+              console.log(err)
+            }
+          }
+        }}
+        fullWidth
+      >
+        シェア
+      </Button>
+    </Container>
   )
 }

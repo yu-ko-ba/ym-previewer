2eb63db685b448efd3c84935d46115396e7f61b6
yu-ko-ba <yu-ko-ba@outlook.com>
2023-01-22 18:12:38 +0900

    TwitterのカードタイプをSummary Cardに変更した

--- src/pages/[musicId].tsx
+++ src/pages/[musicId].tsx
@@ -13,7 +13,7 @@ export const getServerSideProps = async (context: GetServerSidePropsContext) => 
   }
 }
 
-const Music = ({card, title, site, player, playerWidth, playerHeight, image}: PropertiesType) => {
+const Music = ({title, site, player, image}: PropertiesType) => {
   const router = useRouter()
   useEffect(() => {
     router.push(player)
@@ -21,12 +21,9 @@ const Music = ({card, title, site, player, playerWidth, playerHeight, image}: Pr
   return (
     <>
       <Head>
-        <meta name="twitter:card" content={card} />
+        <meta name="twitter:card" content="summary" />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:site" content={site} />
-        <meta name="twitter:player" content={player} />
-        <meta name="twitter:player:width" content={playerWidth} />
-        <meta name="twitter:player:height" content={playerHeight} />
         <meta name="twitter:image" content={image} />
       </Head>
       <p>YouTube Musicへリダイレクト中...</p>
